9d06b81a822f81888f3e465c4060adea46b9250a
rynan4818 <14249877+rynan4818@users.noreply.github.com>
2022-05-30 21:54:08 +0900

    プロジェクトファイルを追加します

---
+++ ChroMapper-MultiDisplayWindow/Plugin.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace ChroMapper_MultiDisplayWindow
+{
+    [Plugin("Multi Display Window")]
+    public class Plugin
+    {
+        [Init]
+        private void Init()
+        {
+            Debug.Log("Multi Display Window Plugin has loaded!");
+            SceneManager.sceneLoaded += SceneLoaded;
+        }
+
+        [Exit]
+        private void Exit()
+        {
+            Debug.Log("Camera Movement:Application has closed!");
+        }
+
+        private void SceneLoaded(Scene arg0, LoadSceneMode arg1)
+        {
+            if (arg0.buildIndex != 3) // Mapper scene
+                return;
+            MapEditorUI mapEditorUI = Object.FindObjectOfType<MapEditorUI>();
+        }
+    }
+}
